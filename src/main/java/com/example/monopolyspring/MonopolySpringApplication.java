377fc861cbec0052a0aac784b7df5f0c352cabe6
Mandloi2309 <tusharmandloi23@gmail.com>
2022-01-28 13:31:00 +0100

    Creating the project

---
+++ src/main/java/com/example/monopolyspring/MonopolySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.monopolyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonopolySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/monopolyspring/MonopolySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monopolyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
